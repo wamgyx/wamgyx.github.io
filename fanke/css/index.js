ebd5d9932dab151cd45a786afc132ed577aa7c7e
wamgyx <1013760630@qq.com>
2017-05-19 11:01:48 +0800

    1

--- fanke/css/index.js
+++ fanke/css/index.js
@@ -11,14 +11,17 @@ $(function(){
 		let h=parseInt(attr(ul[i],"height"));
 		ul[i].setAttribute("h",h);
 		ul[i].style.height=0;
+		ul[i].style.opacity=0;
 	}
 	for(let i=0;i<item.length;i++){
 		if(item[i].children[1]){
 			hover(item[i],function(){
 				item[i].children[1].style.borderColor="#B81C22";
 				animate(item[i].children[1],{height:item[i].children[1].getAttribute("h")},200,function(){});
+				item[i].children[1].style.opacity=1;
 			},function(){
 					animate(item[i].children[1],{height:0},500,function(){item[i].children[1].style.borderColor="#fff";});
+					item[i].children[1].style.opacity=0;
 			})
 		}
 	}
