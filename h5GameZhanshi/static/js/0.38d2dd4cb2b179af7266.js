b9b21c0d3a530d03b608daeb43e958b6bc30a67b
wangyunxia <yunxia.wang@ipaylinks.com>
2021-04-07 18:10:13 +0800

    0407

---
+++ h5GameZhanshi/static/js/0.38d2dd4cb2b179af7266.js
@@ -0,0 +1,2 @@
+webpackJsonp([0],{IkWQ:function(e,t,n){"use strict";Object.defineProperty(t,"__esModule",{value:!0});var s={render:function(){this.$createElement;this._self._c;return this._m(0)},staticRenderFns:[function(){var e=this.$createElement,t=this._self._c||e;return t("div",{staticClass:"about"},[t("h1",[this._v("This is an about page")])])}]},i=n("VU/8")(null,s,!1,null,null,null);t.default=i.exports}});
+//# sourceMappingURL=0.38d2dd4cb2b179af7266.js.map
